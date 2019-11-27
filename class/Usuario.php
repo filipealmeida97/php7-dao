bcf74c17f987450099660292153632bf6d0becf3
Filipe Almeida <filip.goal@gmail.com>
2019-11-27 16:57:01 -0300

    criei métodos de lista e login

--- class/Usuario.php
+++ class/Usuario.php
@@ -42,7 +42,7 @@
 		}
 		//Termino: Métodos sets
 		
-		//Início: Métod para procurar um objeto na base de dados pelo seu 'id'
+		//Início: Método para procurar um objeto na base de dados pelo seu 'id'
 		public function loadById($id){
 
 			$sql = new Sql();
@@ -62,8 +62,49 @@
 
 
 		}
-		//Termino: Métod para procurar um objeto na base de dados pelo seu 'id'
+		//Termino: Método para procurar um objeto na base de dados pelo seu 'id'
 
+		//Início: Método estático que lista todos os usuários/objetos/registros
+		public static function getList(){
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+		}
+		//Termino: Método estático que lista todos os usuários/objetos/registro
+
+		//Início: Método estático que lista usuários/objetos/registro buscando pelo login
+		public static function search($login){
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :DESLOGIN ORDER BY deslogin", array(
+				':DESLOGIN'=>"%".$login."%"
+			));
+		}
+		//Termino: Método estático que lista usuários/objetos/registro buscando pelo login
+
+		public function login($login, $password){
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :DESLOGIN AND dessenha = :DESSENHA", array(
+				":DESLOGIN"=>$login,
+				":DESSENHA"=>$password
+			));
+
+			if (count($results) > 0){
+				$row = $results[0];
+
+				$this->setIdUsuario($row['idusuario']);
+				$this->setDesLogin($row['deslogin']);
+				$this->setDesSenha($row['dessenha']);
+				$this->setDtCadastro(new DateTime($row['dtcadastro']));
+			}else{
+
+				throw new Exception("Usuário não encontrado. Login e/ou senha inválidos");
+
+			}			
+		}
+
+		//Início: Método mágico '__toString'
 		public function __toString(){
 			return json_encode(array(
 				'idusuario'=>$this->getIdUsuario(),
@@ -72,6 +113,7 @@
 				'dtcadastro'=>$this->getDtCadastro()->format("d/m/Y H:i:s")
 			));
 		}
+		//Termino: Método mágico '__toString'
 	}
 	
 ?>
--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$usuario = new Usuario();
+//Nesse traz apenas um usuario e inicia o objeto com seus respectivos atributos e valores
+//$usuario = new Usuario();
+//$usuario->loadById(1);
+//echo $usuario;
 
-$usuario->loadById(1);
 
-echo $usuario;
+//Carrega uma Lista de Usuario
+//$results = Usuario::getList();
+//echo json_encode($results);
 
+//Carrega uma lista de usuario pelo login
+//$search = Usuario::search("ma");
+//echo json_encode($search)
+
+$login = new Usuario();
+
+$login->login("Paulo", "1$%&3");
+
+echo $login
 ?>
